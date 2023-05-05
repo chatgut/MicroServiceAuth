10c261ff883b0fb236662ffefffd84a09041c263
Ye4y <Arman.Alemirad@gmail.com>
2023-05-05 23:15:40 +0200

    Initial commit

---
+++ src/main/java/com/example/microserviceauth/MicroServiceAuthApplication.java
@@ -0,0 +1,15 @@
+package com.example.microserviceauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceAuthApplication.class, args);
+    }
+
+}
+
+//todo: https://www.youtube.com/watch?v=KxqlJblhzfI
---
+++ src/test/java/com/example/microserviceauth/MicroServiceAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/*@SpringBootTest
+class MicroServiceAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}*/
